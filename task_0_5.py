2286d9c491526394748907fa9479205e9bf03843
JuniorBBK <104865217+JuniorBBK@users.noreply.github.com>
2022-05-25 17:14:17 +0200

    Update task_0_5.py

--- task_0_5.py
+++ task_0_5.py
@@ -1,8 +1,8 @@
 def area_of_triangle(int1, int2, int3):
 
-    s = 1/2 * (int1 + int2 + int3)
+    semiperimeter = 1/2 * (int1 + int2 + int3)
 
-    output = (s * (s-int1) * (s-int2) * (s-int3)) ** 0.5
+    output = (semiperimeter * (semiperimeter-int1) * (semiperimeter-int2) * (semiperimeter-int3)) ** 0.5
 
     return output
 
